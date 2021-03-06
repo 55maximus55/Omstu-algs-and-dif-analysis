9d3fc5be2988f784952d8d339108631aadf4d5dd
55_maximus_55 <lukinosh@gmail.com>
2021-03-06 20:20:51 +0600

    Вспомогательные функции, связанные с высотой

--- AVLtree.cpp
+++ AVLtree.cpp
@@ -14,6 +14,25 @@ private:
             height = 1;
         }
     };
+
+    /* Возврат высоты узла */
+    int nodeHeight(Node* node) {
+        return node ? node->height : 0;
+    }
+    /* Восстановление корректного значения высоты заданного узла */
+    void fixNodeHeight(Node* node) {
+        if (node) {
+            int leftHeight = nodeHeight(node->left);
+            int rightHeight = nodeHeight(node->right);
+            node->height = (leftHeight > rightHeight ? leftHeight : rightHeight) + 1;
+        }
+    }
+    /* Разница высот поддеревьев узла */
+    int balanceFactor(Node* node) {
+        if (node)
+            return nodeHeight(node->left) - nodeHeight(node->right);
+        else return 0;
+    }
 public:
     AVLtree() = default;
     void push(key_type key, value_type value);
