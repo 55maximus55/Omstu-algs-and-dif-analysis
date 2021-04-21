8f2606091753be4a3e7c8f644a363809166d90a5
55_maximus_55 <lukinosh@gmail.com>
2021-04-21 06:36:15 +0600

    Task 5 почти сделано

--- task5.cpp
+++ task5.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <cstdlib>
-
+#include "RedBlackTree.cpp"
 using namespace std;
 
 /*
@@ -12,6 +12,12 @@ using namespace std;
  * */
 
 int main() {
-
+    auto tree = RBTree<int, int>();
+    tree.insert(0, 0);
+    tree.insert(1, 0);
+    tree.insert(2, 0);
+    tree.insert(3, 0);
+    tree.insert(4, 0);
+    tree.insert(5, 0);
     return EXIT_SUCCESS;
 }
